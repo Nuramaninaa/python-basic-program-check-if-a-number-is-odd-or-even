9f420564b04d3c5a0c4666234defc815651489a4
Nuramaninaa <60954922+Nuramaninaa@users.noreply.github.com>
2023-07-31 14:27:26 +0800

    Add files via upload

---
+++ number_is_odd_or_even.py
@@ -0,0 +1,12 @@
+#python program to check if a Number is odd or even
+
+
+n = int(input("Enter the number : "))
+
+num = n % 2
+
+if num == 0:
+   print ("n is even number")
+
+else:
+   print ("n is odd number")
